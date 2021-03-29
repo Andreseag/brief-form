a5c387e2e9018f85c4063b13dbbf04798b1eb14f
Andreseag <castrogomezand@gmail.com>
2021-03-28 22:33:39 -0500

    add function next step

--- js/app.js
+++ js/app.js
@@ -1,2 +1,26 @@
 // Variables
 const step = document.querySelector('.step');
+const nextButton = document.querySelector('#next-button');
+const prevButton = document.querySelector('#prev-button');
+//
+let stepPosition = 1;
+
+prevButton.classList.add('d-none');
+//
+nextButton.onclick = () => {
+  const actualStep = document.querySelector(`.step-${stepPosition}`);
+  actualStep.classList.add('d-none');
+  stepPosition += 1;
+  const nextStep = document.querySelector(`.step-${stepPosition}`);
+  nextStep.classList.remove('d-none');
+  prevButton.classList.remove('d-none');
+
+  if (stepPosition === 9) {
+    debugger;
+    nextButton.innerHTML = 'Enviar fomulario';
+  } else {
+    nextButton.innerHTML = 'Siguiente';
+  }
+};
+
+prevButton.onclick = () => {};
